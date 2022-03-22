e7e11a6b9018fe28e47565f8786339927616e74f
vladbbg <vlad.lats@gmail.com>
2022-03-22 16:41:48 +0200

    init

---
+++ precache-manifest.ea69474d1a8c5b890b33c5e687f2012e.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4663fc12b113367b35ea",
+    "url": "/css/app.9a4e5bac.css"
+  },
+  {
+    "revision": "4a634cd1137ab5ede28b",
+    "url": "/css/chunk-05285d3a.445f0ee1.css"
+  },
+  {
+    "revision": "23139ffa37759aa40078",
+    "url": "/css/chunk-1003217c.2c9a6b7d.css"
+  },
+  {
+    "revision": "e68ac6b7509d3d331ec7",
+    "url": "/css/chunk-6f48256a.4f1052b2.css"
+  },
+  {
+    "revision": "ea7813949eefd35bd365",
+    "url": "/css/chunk-vendors.55204a1e.css"
+  },
+  {
+    "revision": "099c63892676bce18742d7326d1eb094",
+    "url": "/img/About/1.jpg"
+  },
+  {
+    "revision": "8ec75477947a519a9da63deba3675a40",
+    "url": "/img/About/2.jpg"
+  },
+  {
+    "revision": "c853831a7d3561119c1a3178cdc09d11",
+    "url": "/img/About/3.jpg"
+  },
+  {
+    "revision": "f3b0582f63945d863d72517bbb7529ef",
+    "url": "/img/About/4.jpg"
+  },
+  {
+    "revision": "72c3fea9caf52b2a4bdb61331b3f6869",
+    "url": "/img/About/5.jpg"
+  },
+  {
+    "revision": "e9ca27fa99e166c1dd74c70c226dc072",
+    "url": "/img/Home/cols-1.jpg"
+  },
+  {
+    "revision": "ddb38cf154e6fe01aac7b3c6ba4625d5",
+    "url": "/img/Home/cols-1.png"
+  },
+  {
+    "revision": "883393741ec2aa5704818523905dda1f",
+    "url": "/img/Home/cols-2.jpg"
+  },
+  {
+    "revision": "7d5e81aa27a351e50c7a8a30c9bece77",
+    "url": "/img/Home/cols-2.png"
+  },
+  {
+    "revision": "0b93cfc75ea7b1ae50f9fab271633764",
+    "url": "/img/Home/cols-3.jpg"
+  },
+  {
+    "revision": "5fc93a2fad677c7cc4a3870675c16b9b",
+    "url": "/img/Home/cols-3.png"
+  },
+  {
+    "revision": "ee3c342e3cce4ac0df9100c854518176",
+    "url": "/img/Home/complete-photo.png"
+  },
+  {
+    "revision": "22859fc5774ee6f72621d67f1c6cc3a3",
+    "url": "/img/Home/main.jpg"
+  },
+  {
+    "revision": "4bc6aaa745f5c42aeef5dc489d726668",
+    "url": "/img/Menu/Starters.jpg"
+  },
+  {
+    "revision": "808f898281ed001b533d6d05d98bea5f",
+    "url": "/img/Menu/Starters.png"
+  },
+  {
+    "revision": "0cd1392933535db8036d01e9c0197d41",
+    "url": "/img/Reservation/1.png"
+  },
+  {
+    "revision": "d1f631604439ca19c000c16f6d3564da",
+    "url": "/img/Reservation/10.png"
+  },
+  {
+    "revision": "e654b10959f9bb20eba3d3a4df61ce63",
+    "url": "/img/Reservation/2.png"
+  },
+  {
+    "revision": "8ecba44f87b0b9ea6c65e9db89ffeeb4",
+    "url": "/img/Reservation/3.png"
+  },
+  {
+    "revision": "e9241590303b0389b1d9814092e903ac",
+    "url": "/img/Reservation/4.png"
+  },
+  {
+    "revision": "ad3dfd07a899df92a38059582e05ff84",
+    "url": "/img/Reservation/5.png"
+  },
+  {
+    "revision": "cd24c456875cf3bc39c75141dd4e5d62",
+    "url": "/img/Reservation/6.png"
+  },
+  {
+    "revision": "421d7dcb13614d3fb2b9b207718c48aa",
+    "url": "/img/Reservation/7.png"
+  },
+  {
+    "revision": "4009c93d2bfca6e4f29c85e8465d23fa",
+    "url": "/img/Reservation/8.png"
+  },
+  {
+    "revision": "749ba2c99bf03e1e2eb0b00cb7c11e5a",
+    "url": "/img/Reservation/9.png"
+  },
+  {
+    "revision": "93faf823ef67250c5306626c9828479d",
+    "url": "/img/main1.jpg"
+  },
+  {
+    "revision": "eaca06ec874f863c1648bd1c2fb46a75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4663fc12b113367b35ea",
+    "url": "/js/app.176b88f5.js"
+  },
+  {
+    "revision": "4a634cd1137ab5ede28b",
+    "url": "/js/chunk-05285d3a.a1fe4b2e.js"
+  },
+  {
+    "revision": "23139ffa37759aa40078",
+    "url": "/js/chunk-1003217c.9ca219ee.js"
+  },
+  {
+    "revision": "4994c1d52d4a902422e9",
+    "url": "/js/chunk-2d0af10a.38974850.js"
+  },
+  {
+    "revision": "329ce684b7a7bddca53a",
+    "url": "/js/chunk-2d0bdda8.3d4ccf3a.js"
+  },
+  {
+    "revision": "99e8e17b08e18a477eb9",
+    "url": "/js/chunk-2d0d0788.449f200c.js"
+  },
+  {
+    "revision": "8a1a8508fa35598bb07d",
+    "url": "/js/chunk-2d0d6cf7.751d9e6d.js"
+  },
+  {
+    "revision": "5b2d0524ff1f47fe409e",
+    "url": "/js/chunk-2d0e17e5.6d509ed2.js"
+  },
+  {
+    "revision": "94075464877b4ad8ae67",
+    "url": "/js/chunk-2d20f410.a4be9cbf.js"
+  },
+  {
+    "revision": "8500e438982f3c7b936d",
+    "url": "/js/chunk-2d217357.336fa6e2.js"
+  },
+  {
+    "revision": "b85f383daf52a6d0dc9a",
+    "url": "/js/chunk-5f4ad635.0a5bb7ef.js"
+  },
+  {
+    "revision": "e68ac6b7509d3d331ec7",
+    "url": "/js/chunk-6f48256a.9a0cfd17.js"
+  },
+  {
+    "revision": "67df0d27c13c8eeb4dac",
+    "url": "/js/chunk-c420df12.3859a06a.js"
+  },
+  {
+    "revision": "ea7813949eefd35bd365",
+    "url": "/js/chunk-vendors.5b33a295.js"
+  },
+  {
+    "revision": "27a30e9a98bef9e1d276d4aff6e75e27",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "a58e933bc2adf8647e18bf0887696f85",
+    "url": "/videos/plaza_time_lapse.mp4"
+  }
+]);
