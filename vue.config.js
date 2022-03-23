054447cf4c9912c14038abbafdef9bf36dbe6ec3
vladbbg <vlad.lats@gmail.com>
2022-03-23 22:42:02 +0200

    change vue.config.jd

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,20 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
   ? '/bianco/'
-  : '/'
+  : '/',
+  pwa: {
+    name: 'My App',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'dev/sw.js',
+      // ...other Workbox options...
+    }
+  }
 }
