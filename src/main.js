26500052f4cf841a78652a814be713cb62041425
vladbbg <vlad.lats@gmail.com>
2022-03-24 20:30:44 +0200

    change node, and dist

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Vuelidate from 'vuelidate'
 import App from './App.vue'
-import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import dateFilter from './filters/date.filter'
